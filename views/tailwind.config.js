276eb6c70b9bb72b00a804284a77a23388a2172f
R2Ksanu <sebsnm2012@gmail.com>
2025-06-12 11:37:10 +0530

    add some pkg

--- views/tailwind.config.js
+++ views/tailwind.config.js
@@ -1,8 +1,28 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './views/**/*.ejs', './public/css/input.css'],
+  content: [
+    './views/**/*.ejs'
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cyan: {
+          600: '#00b7eb',
+          700: '#0099cc'
+        },
+        red: {
+          600: '#e53e3e',
+          700: '#c53030'
+        },
+        green: {
+          600: '#34d399', // Tailwind's default green-600
+          700: '#10b981'  // Tailwind's default green-700
+        },
+        purple: {
+          600: '#a78bfa', // Tailwind's default purple-600
+          700: '#8b5cf6'  // Tailwind's default purple-700
+        }
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
